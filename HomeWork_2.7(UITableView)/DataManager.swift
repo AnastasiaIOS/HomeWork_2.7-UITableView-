f342d0860f5455a29e88f1f36a298af329081543
Anastasia <pavlova.a.s@inbox.ru>
2022-05-05 00:16:32 +0300

    add

---
+++ HomeWork_2.7(UITableView)/DataManager.swift
@@ -0,0 +1,14 @@
+//
+//  DataManager.swift
+//  HomeWork_2.7(UITableView)
+//
+//  Created by Anastasia on 05.05.2022.
+//
+
+class DataManager {
+    private let names = ["Nanaika", "Babaika", "Dendi", "Plumbum", "Fricase", "Keit"]
+    private let surnames = ["Ivanova", "Limonova", "Rudova", "Konkina", "Tupikova", "Koshkina"]
+    private let phones = ["947382", "423422", "342342", "34234332", "4555555", "0329403"]
+    private let emails = ["qqq@bk.ru", "eew@bk.ru", "omo@bk.ru", "qqq@bk.ru", "wwww@bk.ru", "pppp@bk.ru"]
+    
+}
