af7a135056c6f7a1e10c3193e92e0228cb3b5cb7
Anastasia <pavlova.a.s@inbox.ru>
2022-05-05 00:21:43 +0300

    add

---
+++ HomeWork_2.7(UITableView)/Person.swift
@@ -0,0 +1,15 @@
+//
+//  Person.swift
+//  HomeWork_2.7(UITableView)
+//
+//  Created by Anastasia on 05.05.2022.
+//
+
+struct Person {
+    let name: String
+    let surname: String
+    let phone: String
+    let email: String
+}
+
+static 
