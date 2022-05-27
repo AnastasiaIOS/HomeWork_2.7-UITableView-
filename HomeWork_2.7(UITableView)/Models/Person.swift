7d4a199367f493e990b07d5c56a6a47ca74da812
Anastasia <pavlova.a.s@inbox.ru>
2022-05-27 22:20:00 +0300

    add

---
+++ HomeWork_2.7(UITableView)/Models/Person.swift
@@ -0,0 +1,36 @@
+//
+//  Person.swift
+//  HomeWork_2.7(UITableView)
+//
+//  Created by Anastasia on 05.05.2022.
+//
+
+struct Person {
+    let name: String
+    let surname: String
+    let phone: String
+    let email: String
+    
+    var title: String {
+        "\(name) \(surname)"
+    }
+}
+extension Person {
+    static func getPerson() -> [Person] {
+        
+        var massiveOfPersons: [Person] = []
+        
+        for _ in 1...numberOfPersons {
+            massiveOfPersons.append (Person(name: groupMassive.names.randomElement() ?? "",
+                                            surname: groupMassive.surnames.randomElement() ?? "",
+                                            phone: groupMassive.phones.randomElement() ?? "",
+                                            email: groupMassive.emails.randomElement() ?? ""))
+                                     
+        }
+        return massiveOfPersons
+    }
+}
+                                     
+                                     
+                                     
+                                     
--- HomeWork_2.7(UITableView)/ServiceFile/DataManager.swift
+++ HomeWork_2.7(UITableView)/ServiceFile/DataManager.swift
@@ -6,12 +6,17 @@
 //
 
 class DataManager {
+     
+
      let names = ["Nanaika", "Babaika", "Dendi", "Plumbum", "Fricase", "Keit"]
      let surnames = ["Ivanova", "Limonova", "Popova", "Konkina", "Tupikova", "Koshkina"]
      let phones = ["947382", "423422", "342342", "34234332", "4555555", "0329403"]
      let emails = ["qqq@bk.ru", "eew@bk.ru", "omo@bk.ru", "qqq@bk.ru", "wwww@bk.ru", "pppp@bk.ru"]
     
+     //let numberOfPersons = names.count
 }
 
 let groupMassive = DataManager()
+let numberOfPersons = groupMassive.names.count
+
 
