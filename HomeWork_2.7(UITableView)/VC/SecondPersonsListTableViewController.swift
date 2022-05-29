5b684c156bb61d2df3573309ae8579e41a80d86b
Anastasia <pavlova.a.s@inbox.ru>
2022-05-30 01:46:50 +0300

    add

--- HomeWork_2.7(UITableView)/VC/SecondPersonsListTableViewController.swift
+++ HomeWork_2.7(UITableView)/VC/SecondPersonsListTableViewController.swift
@@ -21,11 +21,11 @@ class SecondPersonsListTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        // #warning Incmplete implementation, return the number of rows
         return 0
     }
 
--- StartScreenOfPersons.swift
+++ StartScreenOfPersons.swift
@@ -15,7 +15,7 @@ class StartScreenOfPersons: UITabBarController {
     func goToViewControllers() {
         let persons = Person.getPerson()
         let personsListVC = viewControllers?.first as! PersonsListViewController
-        let secondListVC = viewControllers?.last as! SecondListOfPersons
+        let secondListVC = viewControllers?.last as! SecondPersonsListTableViewController
         
         // !!!!ВОПРОС: КАК ПОНЯТЬ ЧТО ПОСЛЕ viewControllers НУЖНО БЫЛО СТАВИТЬ "?." И first (В ОДНОМ СЛУЧАЕ) И Last в ДРУГОМ СЛУЧАЕ? это где-то в документации есть?
         
