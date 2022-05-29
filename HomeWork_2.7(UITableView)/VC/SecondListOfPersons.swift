718b586c3c218161f1641171c8187b287e787489
Anastasia <pavlova.a.s@inbox.ru>
2022-05-30 00:31:53 +0300

    add

---
+++ HomeWork_2.7(UITableView)/VC/SecondListOfPersons.swift
@@ -0,0 +1,12 @@
+//
+//  SecondListOfPersons.swift
+//  HomeWork_2.7(UITableView)
+//
+//  Created by Anastasia on 30.05.2022.
+//
+
+import UIKit
+
+class SecondListOfPersons: UITableViewController {
+    
+}
---
+++ StartScreenOfPersons.swift
@@ -0,0 +1,21 @@
+//
+//  StartScreenOfPersons.swift
+//  HomeWork_2.7(UITableView)
+//
+//  Created by Anastasia on 30.05.2022.
+//
+
+import UIKit
+
+class StartScreenOfPersons: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func goToViewControllers() {
+        let persons = Person.getPerson()
+        let personsListVC = viewControllers?.first as! PersonsListViewController
+        let secondListVC = viewControllers?.last as!
+        
+    }
+}
